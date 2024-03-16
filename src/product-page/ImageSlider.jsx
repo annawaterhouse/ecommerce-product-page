13edb976a31f3fb565d9098e07df56f62dbf508f
Anna Waterhouse <annawaterhouse05@gmail.com>
2024-03-16 17:47:09 -0400

    image slider previews added

--- src/product-page/ImageSlider.jsx
+++ src/product-page/ImageSlider.jsx
@@ -26,37 +26,50 @@ export default function ImageSlide() {
       setCurrIndex(currIndex - 1);
     }
   };
+  //image previews hover and click functionality
+  const imageChange = (index) => {
+    setCurrIndex(index);
+  }
   return (
     <>
-    <section className="slider">
-      <div className="arrows">
-        <button className="left" onClick={prevImage}>
-          <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M11 1 3 9l8 8"
-              stroke="#1D2026"
-              strokeWidth="3"
-              fill="none"
-              fillRule="evenodd"
-            />
-          </svg>
-        </button>
-        <button className="right" onClick={nextImage}>
-          <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="m2 1 8 8-8 8"
-              stroke="#1D2026"
-              strokeWidth="3"
-              fill="none"
-              fillRule="evenodd"
-            />
-          </svg>
-        </button>
-      </div>
-      <img src={images[currIndex]} alt="sneaker[currIndex]" />
-
-    </section>
-    
+      <section className="slider">
+        <div className="arrows slider_arrows">
+          <button className="left" onClick={prevImage}>
+            <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M11 1 3 9l8 8"
+                stroke="#1D2026"
+                strokeWidth="3"
+                fill="none"
+                fillRule="evenodd"
+              />
+            </svg>
+          </button>
+          <button className="right" onClick={nextImage}>
+            <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m2 1 8 8-8 8"
+                stroke="#1D2026"
+                strokeWidth="3"
+                fill="none"
+                fillRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+        <img src={images[currIndex]} alt={`sneakerHighlight${images[currIndex]}`} />
+        <section className="slider_preview">
+          {images.map((image, index) => (
+            <div className={currIndex === index ? 'active' : ''} key={index} onClick={()=>imageChange(index)}>
+              <img
+                src={image}
+                alt={`sneaker${index}`}
+              />
+            </div>
+          )
+          )}
+        </section>
+      </section>
     </>
   );
 }
